ea2d1d04c266e53056880deb595e7afc36392850
Hamsiny <459516309@qq.com>
2021-07-20 16:51:35 +1200

    update validator email

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -29,7 +29,8 @@ export class FormComponent implements OnInit {
       ]),
       email: new FormControl('', [
         Validators.required,
-        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        // Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        Validators.email,
       ]),
       phoneNumber: new FormControl('', [
         Validators.required,
